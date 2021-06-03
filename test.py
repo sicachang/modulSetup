01f78b5fae4ce98eca1d63d24711281d4c2e1212
sicachang <31688096+sicachang@users.noreply.github.com>
2021-06-03 23:46:00 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,4 @@
+#!D:\Anaconda3\envs\aidc\python.exe
+# EASY-INSTALL-SCRIPT: 'mytest==0.30','test.py'
+__requires__ = 'mytest==0.30'
+__import__('pkg_resources').run_script('mytest==0.30', 'test.py')

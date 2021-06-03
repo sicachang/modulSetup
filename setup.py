ef2cb0b5b4843709aec1eb2d610b438b660dbd94
sicachang <31688096+sicachang@users.noreply.github.com>
2021-06-03 23:49:52 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
       name="mytest",
       version="0.30",
-      description="My test module",
+      description="Station Object for AIEXPL PROJECT",
       author="Sica Chang",
       url="http://www.csdn.net",
       license="LGPL",
